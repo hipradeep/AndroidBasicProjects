70ceb8211cc856618e06b49c6a82ec72838b3ef5
Pradeep <pradeepmaurya.cse@gmail.com>
2022-02-01 11:04:15 +0530

    add bottom nav with drawer

---
+++ app/src/main/java/com/hipradeep/learnandroid/bottomnavigationbar/BottomNavigationWithDrawerMenuActivity.java
@@ -0,0 +1,16 @@
+package com.hipradeep.learnandroid.bottomnavigationbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.hipradeep.learnandroid.R;
+
+public class BottomNavigationWithDrawerMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom_navigation_with_drawer_menu);
+    }
+}
